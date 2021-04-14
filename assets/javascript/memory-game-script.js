f99b1a74e639e73618a36f7181055003ca32bf39
davidhuk <73908391+davidhuk@users.noreply.github.com>
2021-04-14 15:02:56 +0000

    Updated names of CSS class. Added JavaScript to store datatype of clicked item.

--- assets/javascript/memory-game-script.js
+++ assets/javascript/memory-game-script.js
@@ -1,5 +1,8 @@
 function onCardClicked(event) {
     let cardClicked = event.currentTarget;
     cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked.*/
-    cardClicked.className += ' user-matched-cards';/*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
+    cardClicked.className += ' user-matched-first-card'; /*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
+    firstCardClickedDataType = cardClicked.getAttribute("data-card-color"); /*Need this variable to use to apply comparing logic to compare 1st and second click if true for match*/
+
+    console.log(firstCardClickedDataType);/*Testing if console logs data type on click of square*/
 }
