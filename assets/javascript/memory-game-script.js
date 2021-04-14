f53793d5a962747ec96f19582970c712f0e079cc
davidhuk <73908391+davidhuk@users.noreply.github.com>
2021-04-14 13:51:05 +0000

    Updated JavaScript testing matching colour on 1st & 2nd clicks.

--- assets/javascript/memory-game-script.js
+++ assets/javascript/memory-game-script.js
@@ -1,8 +1,14 @@
+var userClickedCard = null;
+
 function onCardClicked(event) {
     let cardClicked = event.currentTarget;
+    cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked. Needed to move this code outside of the below loop. The 'black' css class needs to be remove every time the user clicks a square, including between 1st & 2nd click for colour match.*/
 
-    if(cardClicked) {
-        /*removes the 'black' css class from square clicked*/
-        cardClicked.className = cardClicked.className.replace('black', '').trim();
+    if(!userClickedCard) {
+        userClickedCard = cardClicked; /*Keeps track of when a card is clicked.*/
+    } else if (cardClicked) {
+        if (userClickedCard.getAttribute('data-card-color') === cardClicked.getAttribute('data-card-color')) /*Compares the data type of the 2 x clicked squares to determine a valid colour match.*/ {
+            console.log('This is a test to check if logic is working correctly so far.'); 
+        }
     }
 }
