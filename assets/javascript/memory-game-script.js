ec09c421c9a1bbb00f4db566e09156eef6a71c27
davidhuk <73908391+davidhuk@users.noreply.github.com>
2021-04-14 15:57:51 +0000

    Added & tested conditonal logic for 1st & 2nd click to JavaScript.

--- assets/javascript/memory-game-script.js
+++ assets/javascript/memory-game-script.js
@@ -1,8 +1,15 @@
+var clickedCard = null; /*Needed a default JavaScript type of null to run some logic at 1st & 2nd clicks. Documentation here - https://www.w3schools.com/js/js_datatypes.asp*/
+
 function onCardClicked(event) {
-    let cardClicked = event.currentTarget;
+    const cardClicked = event.currentTarget;
     cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked.*/
     cardClicked.className += ' user-matched-first-card'; /*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
-    firstCardClickedDataType = cardClicked.getAttribute("data-card-color"); /*Need this variable to use to apply comparing logic to compare 1st and second click if true for match*/
 
-    console.log(firstCardClickedDataType);/*Testing if console logs data type on click of square*/
+    if (!clickedCard) {
+        clickedCard = cardClicked
+        let userFirstCardClickDataType = cardClicked.getAttribute('data-card-color');
+        console.log(userFirstCardClickDataType);
+    } else if (clickedCard) {
+        console.log(cardClicked.getAttribute('data-card-color'));/*Testing that logic works*/
+    }
 }
