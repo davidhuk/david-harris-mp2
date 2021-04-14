f0c8c570703bb39c0736764ac95b27293d4896ea
davidhuk <73908391+davidhuk@users.noreply.github.com>
2021-04-14 14:26:30 +0000

    Added CSS class to disable clicking square after first click. Stripped back JavaScript logic to get CSS class additions working on click.

--- assets/javascript/memory-game-script.js
+++ assets/javascript/memory-game-script.js
@@ -1,14 +1,5 @@
-var userClickedCard = null;
-
 function onCardClicked(event) {
     let cardClicked = event.currentTarget;
-    cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked. Needed to move this code outside of the below loop. The 'black' css class needs to be remove every time the user clicks a square, including between 1st & 2nd click for colour match.*/
-
-    if(!userClickedCard) {
-        userClickedCard = cardClicked; /*Keeps track of when a card is clicked.*/
-    } else if (cardClicked) {
-        if (userClickedCard.getAttribute('data-card-color') === cardClicked.getAttribute('data-card-color')) /*Compares the data type of the 2 x clicked squares to determine a valid colour match.*/ {
-            console.log('This is a test to check if logic is working correctly so far.'); 
-        }
-    }
+    cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked.*/
+    cardClicked.className += ' user-matched-cards';/*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
 }
