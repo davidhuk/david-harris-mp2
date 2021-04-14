19b779fb0246d6148f9c764c1661160c33684a73
davidhuk <73908391+davidhuk@users.noreply.github.com>
2021-04-14 16:50:35 +0000

    Update JavaScript with logic loop for 1st & 2nd clicks to gather coluor data.

--- assets/javascript/memory-game-script.js
+++ assets/javascript/memory-game-script.js
@@ -1,15 +1,18 @@
-var clickedCard = null; /*Needed a default JavaScript type of null to run some logic at 1st & 2nd clicks. Documentation here - https://www.w3schools.com/js/js_datatypes.asp*/
+var notClicked = null; /*Needed a default JavaScript type of null to run some logic at 1st & 2nd clicks. Documentation here - https://www.w3schools.com/js/js_datatypes.asp*/
 
 function onCardClicked(event) {
-    const cardClicked = event.currentTarget;
-    cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked.*/
-    cardClicked.className += ' user-matched-first-card'; /*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
-
-    if (!clickedCard) {
-        clickedCard = cardClicked
-        let userFirstCardClickDataType = cardClicked.getAttribute('data-card-color');
-        console.log(userFirstCardClickDataType);
-    } else if (clickedCard) {
-        console.log(cardClicked.getAttribute('data-card-color'));/*Testing that logic works*/
+    let cardClicked = event.currentTarget;
+    
+    if (!notClicked) {
+        cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked.*/
+        cardClicked.className += ' user-matched-first-card'; /*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
+        firstCardClickedDataColorValue = cardClicked.getAttribute('data-card-color');
+        console.log(firstCardClickedDataColorValue);
+        notClicked = true;
+    } else if (notClicked) {   
+        cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked.*/
+        cardClicked.className += ' user-matched-second-card'; /*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
+        notClicked = null;
+        console.log('This should be the second colour click.');
     }
 }
