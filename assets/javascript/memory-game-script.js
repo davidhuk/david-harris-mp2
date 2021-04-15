3986d628563b953a3eb95874f30bbc838cf22564
davidhuk <73908391+davidhuk@users.noreply.github.com>
2021-04-15 12:19:44 +0000

    Updated CSS class names. Updated JavaScript to include null and target. Matched logic was not working without these.

--- assets/javascript/memory-game-script.js
+++ assets/javascript/memory-game-script.js
@@ -1,18 +1,19 @@
-var notClicked = null; /*Needed a default JavaScript type of null to run some logic at 1st & 2nd clicks. Documentation here - https://www.w3schools.com/js/js_datatypes.asp*/
+var cardClicked = null; /*Needed to add null to enable logic for rule later to run on 1st, then 2nd click for color match logic for the game.*/
 
 function onCardClicked(event) {
-    let cardClicked = event.currentTarget;
-    
-    if (!notClicked) {
-        cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked.*/
-        cardClicked.className += ' user-matched-first-card'; /*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
-        firstCardClickedDataColorValue = cardClicked.getAttribute('data-card-color');
-        console.log(firstCardClickedDataColorValue);
-        notClicked = true;
-    } else if (notClicked) {   
-        cardClicked.className = cardClicked.className.replace('black', '').trim(); /*Removes the 'black' css class from square clicked.*/
-        cardClicked.className += ' user-matched-second-card'; /*Add's 'user-matched-cards' css class to clicked square. This css class will disable clicking the same square.*/
-        notClicked = null;
-        console.log('This should be the second colour click.');
+    let target = event.currentTarget;
+    target.className = target.className.replace("black", "").trim(); /*Removes "black" css class from clicked square.*/
+    target.className += " user-matched-card"; /*Add css class to clicked square.*/
+
+    if (!cardClicked) {
+        console.log(target.getAttribute("data-card-color"));
+        console.log("This should be the first click color.") /*Testing logic works.*/
+        cardClicked = target;
+    } else if (cardClicked) {
+        
+        if (cardClicked.getAttribute("data-card-color") === target.getAttribute("data-card-color")) {
+            console.log(cardClicked.getAttribute("data-card-color"));
+            console.log("This is a confirmed match of 1st & 2nd color");/*Testing logic works.*/
+        }
     }
 }
